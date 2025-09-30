69915811c98d63baad30592198096127ff0f62de
Otto Jussila <e2301736@edu.vamk.fi>
2025-09-30 22:35:24 +0300

    Add project files.

---
+++ GlobalXmlns.cs
@@ -0,0 +1,2 @@
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "HelloWorld")]
+[assembly: XmlnsDefinition("http://schemas.microsoft.com/dotnet/maui/global", "HelloWorld.Pages")]
